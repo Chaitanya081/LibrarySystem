41ef5d0abdafdb86d480d754d0d23c06fbd974ce
Chaitanya081 <192424185.simats@saveetha.com>
2026-01-29 14:10:57 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,68 @@
 #include <iostream>
-#include "user.h"
+#include "auth.h"
 using namespace std;
+
+// Function declarations
+void adminMenu();
+void userMenu();
 
 int main() {
     User currentUser;
-    int choice;
+    string username, password;
 
-    cout << "1. Login\n2. Exit\n";
-    cin >> choice;
+    cout << "==== Library Management System ====\n";
+    cout << "Username: ";
+    cin >> username;
+    cout << "Password: ";
+    cin >> password;
 
-    if (choice == 1) {
-        string u, p;
-        cout << "Username: ";
-        cin >> u;
-        cout << "Password: ";
-        cin >> p;
+    if (login(username, password, currentUser)) {
+        cout << "\nLogin Successful!\n";
 
-        if (login(u, p, currentUser)) {
-            cout << "Welcome " << currentUser.username << endl;
+        if (currentUser.role == "admin") {
+            adminMenu();
         } else {
-            cout << "Invalid credentials\n";
+            userMenu();
         }
+    } else {
+        cout << "\nInvalid Login Credentials\n";
     }
+
     return 0;
+}
+
+void adminMenu() {
+    int choice;
+    do {
+        cout << "\n--- Admin Menu ---\n";
+        cout << "1. Add Book\n";
+        cout << "2. View Books\n";
+        cout << "3. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            addBook();
+            break;
+        case 2:
+            viewBooks();
+            break;
+        }
+    } while (choice != 3);
+}
+
+void userMenu() {
+    int choice;
+    do {
+        cout << "\n--- User Menu ---\n";
+        cout << "1. View Books\n";
+        cout << "2. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            viewBooks();
+        }
+    } while (choice != 2);
 }
